4965098064102981cc9e12d9a62a626b6fa1236e
Wallace He <guanlin.he@richmond.edu>
2019-04-09 13:10:55 -0400

    Adding Multi thread
    Adding Multi thread

--- demo.py
+++ demo.py
@@ -4,42 +4,170 @@ import imutils
 import time
 import cv2
 import os
+from __future__ import print_function
+
+import numpy as np
+
+from multiprocessing.pool import ThreadPool
+from collections import deque
 
 
+
+
+class Video:
+    def __init__(self, videoAddress,configs):
+        if configs['mutiThread']:
+            self.applyMutiThread=True
+        else: self.applyMutiThread=False
+        self.writeVideo=False
+        self.threadn = 1
+        self.pool = dests
+        self.pending = deque()
+        self.videoReader= cv2.VideoCapture(videoAddress)
+    def videoProcess(model):
+        writer = None
+        (W, H) = (None, None)
+        try:
+            prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
+                else cv2.CAP_PROP_FRAME_COUNT
+            total = int(self.videoReader.get(prop))
+            print("[INFO] {} total frames in video".format(total))
+
+        # an error occurred while trying to determine the total
+        # number of frames in the video file
+        except:
+            print("[INFO] could not determine # of frames in video")
+            print("[INFO] no approx. completion time can be provided")
+            total = -1
+         while True:
+            while len(pending) > 0 and pending[0].ready():
+                (grabbed, frame) = vs.read()
+    def videoToFrame(configs):
+        return 1
+
+
+
+class Model:
+    def __init__(self, Task, configs):
+        self.modelNet = None
+        self.ln=None
+        self.modelType = Task
+        if config["class"]:
+            self.labels = open(config["class"]).read().strip().split("\n")
+    def loadmodel():
+        return 1
+
+    def process_frame(frame):
+        COLORS=  np.random.randint(0, 255, size=(len(self.labels), 3),dtype="uint8")
+        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+            swapRB=True, crop=False)
+        self.modelNet.setInput(blob)
+        start = time.time()
+        layerOutputs = self.modelNet.forward(ln)
+        end = time.time()
+        boxes = []
+        confidences = []
+        classIDs = []
+        for output in layerOutputs:
+            # loop over each of the detections
+            for detection in output:
+            # extract the class ID and confidence (i.e., probability)
+            # of the current object detection
+                scores = detection[5:]
+                classID = np.argmax(scores)
+                confidence = scores[classID]
+
+                # filter out weak predictions by ensuring the detected
+                # probability is greater than the minimum probability
+                if confidence > args["confidence"]:
+                    # scale the bounding box coordinates back relative to
+                    # the size of the image, keeping in mind that YOLO
+                    # actually returns the center (x, y)-coordinates of
+                    # the bounding box followed by the boxes' width and
+                    # height
+                    box = detection[0:4] * np.array([W, H, W, H])
+                    (centerX, centerY, width, height) = box.astype("int")
+
+                    # use the center (x, y)-coordinates to derive the top
+                    # and and left corner of the bounding box
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
+
+                    # update our list of bounding box coordinates,
+                    # confidences, and class IDs
+                    boxes.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))
+                    classIDs.append(classID)
+
+
+            # apply non-maxima suppression to suppress weak, overlapping
+            # bounding boxes
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, self.confidence,self.threshold)
+
+        # ensure at least one detection exists
+        output = {}
+        if len(idxs) > 0:
+            # loop over the indexes we are keeping
+            for i in idxs.flatten():
+                # extract the bounding box coordinates
+                x, y = boxes[i][0], boxes[i][1]
+                w, h = boxes[i][2], boxes[i][3]
+
+                # draw a bounding box rectangle and label on the frame
+                if self.write==True:
+                    color = [int(c) for c in COLORS[classIDs[i]]]
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                    text = "{}: {:.4f}".format(self.labels[classIDs[i]],
+                        confidences[i])
+                    cv2.putText(frame, text, (x, y - 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                output.append([])
+        # check if the video writer is None
+        if writer is None:
+            # initialize our video writer
+            fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+            writer = cv2.VideoWriter(self."output", fourcc, 30,
+                (frame.shape[1], frame.shape[0]), True)
+            writer.write(frame)
+            # some information on processing single frame
+            #if total > 0:
+            #    elap = (end - start)
+            #    print("[INFO] single frame took {:.4f} seconds".format(elap))
+            #    print("[INFO] estimated total time to finish: {:.4f}".format(
+            #        elap * total))
+
+        # write the output frame to disk
+        return 
+
+    # some intensive computation...
+    
+        
+        
 
 def videoAnalysis(videos,task,config):
     
-    LABELS=open(config["class"]).read().strip().split("\n")
-    COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
-	dtype="uint8")
-    weightsPath = os.path.sep.join([config["model"], task+".weights"])
-    configPath = os.path.sep.join([config["model"], task+".cfg"])
+    video=Video(videos,configs)
+    model=Models(Task,config)
+    
+    
+    threadn = cv.getNumberOfCPUs()
+    pool = ThreadPool(processes = threadn)
+    
+
     if task=="yolo":
+        weightsPath = os.path.sep.join([config["model"], task+".weights"])
+        configPath = os.path.sep.join([config["model"], task+".cfg"])
         net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
         ln = net.getLayerNames()
         ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
         # initialize the video stream, pointer to output video file, and
         # frame dimensions
-        vs = cv2.VideoCapture(config["input"])
-        writer = None
-        (W, H) = (None, None)
-        try:
-	        prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
-		        else cv2.CAP_PROP_FRAME_COUNT
-	        total = int(vs.get(prop))
-	        print("[INFO] {} total frames in video".format(total))
-
-        # an error occurred while trying to determine the total
-        # number of frames in the video file
-        except:
-	        print("[INFO] could not determine # of frames in video")
-	        print("[INFO] no approx. completion time can be provided")
-	        total = -1
+        #vs = cv2.VideoCapture(config["input"])
+       
         #need to change to muti processing mode 
 
-        while True:
-            (grabbed, frame) = vs.read()
+
 	        # if the frame was not grabbed, then we have reached the end
 	        # of the stream
             if not grabbed:
@@ -52,88 +180,11 @@ def videoAnalysis(videos,task,config):
 	        # construct a blob from the input frame and then perform a forward
 	        # pass of the YOLO object detector, giving us our bounding boxes
 	        # and associated probabilities
-	        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-		    swapRB=True, crop=False)
-	        net.setInput(blob)
-            start = time.time()
-            layerOutputs = net.forward(ln)
-            end = time.time()
+	        
 
             # initialize our lists of detected bounding boxes, confidences,
             # and class IDs, respectively
-            boxes = []
-            confidences = []
-            classIDs = []
-            for output in layerOutputs:
-		# loop over each of the detections
-                for detection in output:
-                    # extract the class ID and confidence (i.e., probability)
-                    # of the current object detection
-                    scores = detection[5:]
-                    classID = np.argmax(scores)
-                    confidence = scores[classID]
-
-                    # filter out weak predictions by ensuring the detected
-                    # probability is greater than the minimum probability
-                    if confidence > args["confidence"]:
-                        # scale the bounding box coordinates back relative to
-                        # the size of the image, keeping in mind that YOLO
-                        # actually returns the center (x, y)-coordinates of
-                        # the bounding box followed by the boxes' width and
-                        # height
-                        box = detection[0:4] * np.array([W, H, W, H])
-                        (centerX, centerY, width, height) = box.astype("int")
-
-                        # use the center (x, y)-coordinates to derive the top
-                        # and and left corner of the bounding box
-                        x = int(centerX - (width / 2))
-                        y = int(centerY - (height / 2))
-
-                        # update our list of bounding box coordinates,
-                        # confidences, and class IDs
-                        boxes.append([x, y, int(width), int(height)])
-                        confidences.append(float(confidence))
-                        classIDs.append(classID)
-
-
-            # apply non-maxima suppression to suppress weak, overlapping
-	        # bounding boxes
-	        idxs = cv2.dnn.NMSBoxes(boxes, confidences, config["confidence"],
-		        config["threshold"])
-
-	        # ensure at least one detection exists
-            if len(idxs) > 0:
-                # loop over the indexes we are keeping
-                for i in idxs.flatten():
-                    # extract the bounding box coordinates
-                    (x, y) = (boxes[i][0], boxes[i][1])
-                    (w, h) = (boxes[i][2], boxes[i][3])
-
-                    # draw a bounding box rectangle and label on the frame
-                    color = [int(c) for c in COLORS[classIDs[i]]]
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-                    text = "{}: {:.4f}".format(LABELS[classIDs[i]],
-                        confidences[i])
-                    cv2.putText(frame, text, (x, y - 5),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-            # check if the video writer is None
-            if writer is None:
-                # initialize our video writer
-                fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-                writer = cv2.VideoWriter(config["output"], fourcc, 30,
-                    (frame.shape[1], frame.shape[0]), True)
-
-                # some information on processing single frame
-                if total > 0:
-                    elap = (end - start)
-                    print("[INFO] single frame took {:.4f} seconds".format(elap))
-                    print("[INFO] estimated total time to finish: {:.4f}".format(
-                        elap * total))
-
-            # write the output frame to disk
-            writer.write(frame)
-
+            
         # release the file pointers
         print("[INFO] cleaning up...")
         writer.release()
@@ -161,5 +212,6 @@ if __name__ == '__main__':
     ap.add_argument("-t", "--threshold", type=float, default=0.3,
 	help="threshold when applyong non-maxima suppression")
     args = vars(ap.parse_args())
-    videoAnalysis(args["video"],args["task"],args)
+    Result_File, WrittenVideo= cv2.video_DNN.videoAnalysis(videoes, Tasks, configs). videoAnalysis(args["video"],args["task"],args)
+
 
